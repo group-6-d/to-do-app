bee21ba6a6e28b28b758f19edefc70825caaf35f
elen-oz <ozerova.lenka@gmail.com>
2024-03-06 17:42:46 +0100

    feat: add side menu

--- frontend/src/components/MainSection/MainSection.tsx
+++ frontend/src/components/MainSection/MainSection.tsx
@@ -1,4 +1,24 @@
 const MainSection = () => {
-  return <main className='h-full bg-slate-300'>MainSection</main>;
+  return (
+    <main className='flex h-full bg-slate-200'>
+      <aside className='border-2 border-gray-300'>
+        <button className='mb-4 bg-indigo-700 px-8 py-4 text-white'>
+          New Task
+        </button>
+        <ul className='pl-5'>
+          <li className='py-2 hover:cursor-pointer hover:underline'>
+            Personal
+          </li>
+          <li className='py-2 hover:cursor-pointer hover:underline'>Work</li>
+          <li className='py-2 hover:cursor-pointer hover:underline'>
+            Shopping
+          </li>
+          <li className='py-2 hover:cursor-pointer hover:underline'>Hobbies</li>
+          <li className='py-2 hover:cursor-pointer hover:underline'>Movies</li>
+        </ul>
+      </aside>
+      <div></div>
+    </main>
+  );
 };
 export default MainSection;
