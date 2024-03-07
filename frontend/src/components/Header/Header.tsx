db89ba59ba13e1264eec615692d006def2b9324f
elen-oz <ozerova.lenka@gmail.com>
2024-03-07 20:27:27 +0100

    feat: change header view: add theme toggle

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,4 +1,6 @@
 import { Link, NavLink } from 'react-router-dom';
+import { MdOutlineDarkMode } from 'react-icons/md';
+
 import styles from './Header.module.css';
 
 type NavLinkWithStyleProps = {
@@ -12,28 +14,33 @@ const NavLinkWithStyle = ({ isActive, label }: NavLinkWithStyleProps) => (
 
 const Header = () => {
   return (
-    <header className='flex h-[10%] w-full items-center justify-between px-8'>
-      <h2 className=''>
-        <Link to='/'>TODO App</Link>
-      </h2>
-      <nav>
-        <ul className='flex justify-between gap-4'>
-          <li>
-            <NavLink to='/' end>
-              {({ isActive }) => (
-                <NavLinkWithStyle isActive={isActive} label='Dashboard' />
-              )}
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/profile'>
-              {({ isActive }) => (
-                <NavLinkWithStyle isActive={isActive} label='Profile' />
-              )}
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <header className='flex h-[10%] w-full items-center justify-between bg-stone-100 px-8'>
+      <Link to='/'>
+        <h2 className=''>TODO App</h2>
+      </Link>
+      <div className='flex items-center'>
+        <div className='cursor-pointer p-4 text-stone-400 hover:text-stone-700'>
+          <MdOutlineDarkMode className='pt-1 text-3xl' />
+        </div>
+        <nav>
+          <ul className='flex justify-between gap-4 rounded-full bg-white px-6 py-3 shadow-xl'>
+            <li>
+              <NavLink to='/' end>
+                {({ isActive }) => (
+                  <NavLinkWithStyle isActive={isActive} label='Dashboard' />
+                )}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/profile'>
+                {({ isActive }) => (
+                  <NavLinkWithStyle isActive={isActive} label='Profile' />
+                )}
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
