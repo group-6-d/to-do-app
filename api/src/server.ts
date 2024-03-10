d779e9f667b407957afc54f176569e0aabfb4dce
Jinjing Wu <jinjing.wu@compass.com>
2024-03-10 11:20:16 +0100

    feat: db setup

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import { destroyDBConnections } from './client/db';
 import app from './app';
 
 dotenv.config();
@@ -7,6 +8,20 @@ const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+const shutdown = async (signal: string) => {
+  await destroyDBConnections();
+  console.info(`[${signal}] This service is shutting down!`);
+  process.exit(0);
+};
+
+process.on('SIGTERM', async () => {
+  await shutdown('SIGTERM');
+});
+
+process.on('SIGINT', async () => {
+  await shutdown('SIGINT');
 });
 
 export default app;
