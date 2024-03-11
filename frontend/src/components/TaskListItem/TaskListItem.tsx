dd7d059166733e6c1164e60740f70387795e8862
elen-oz <ozerova.lenka@gmail.com>
2024-03-11 11:47:41 +0100

    feat: add priority flag to item in dashboard view

--- frontend/src/components/TaskListItem/TaskListItem.tsx
+++ frontend/src/components/TaskListItem/TaskListItem.tsx
@@ -7,7 +7,16 @@ interface TaskProps {
 }
 
 const TaskListItem: FC<TaskProps> = ({ task, onClick }) => {
-  const { id, title, category } = task;
+  const { id, title, category, priority } = task;
+
+  let priorityColor: string;
+  if (priority === 'high priority') {
+    priorityColor = 'bg-orange';
+  } else if (priority === 'middle priority') {
+    priorityColor = 'bg-yellow';
+  } else {
+    priorityColor = 'bg-purple';
+  }
 
   return (
     <li
@@ -15,7 +24,11 @@ const TaskListItem: FC<TaskProps> = ({ task, onClick }) => {
       onClick={() => onClick(task)}
       className='hover:dark:border-accent mx-2 mb-3 flex flex-col justify-between rounded-lg border-[1px] border-stone-200 bg-white px-4 py-2 hover:bg-stone-100 dark:border-stone-700 dark:bg-stone-800'
     >
-      <div className='font-thin text-stone-400'>&#8250;&#32;{category}</div>
+      <div className='flex items-center justify-between'>
+        <div className='font-thin text-stone-400'>&#8250;&#32;{category}</div>
+        <div className={`h-[1rem] w-[1rem] rounded-full ${priorityColor}`} />
+      </div>
+
       <div className='flex items-center justify-between'>
         <h3 className='py-4 pr-4'>{title}</h3>
 
