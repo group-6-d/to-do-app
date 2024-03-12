c5c2d225bd8acf11ad1931db13c2e175aa35cd26
elen-oz <ozerova.lenka@gmail.com>
2024-03-12 16:21:33 +0100

    feat: add filter by date &  add dates to fake task list

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,17 +1,43 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import SideBar from '../../components/SideBar/SideBar';
 import TaskList from '../../components/TaskList/TaskList';
 import useTasksBoard from '../../providers/TasksProvider/TasksProvider.hook';
 import { useDates } from '../../hooks/useDates';
+import type TaskCard from '../../models/TaskCard';
 
 const MainPage = () => {
   const { getTasksList, taskListDate, getCategoryList, categoryListDate } =
     useTasksBoard();
   const { formattedDateToday, formattedDateTomorrow } = useDates();
+  const [tasksToday, setTasksToday] = useState<TaskCard[]>([]);
+  const [tasksTomorrow, setTasksTomorrow] = useState<TaskCard[]>([]);
+  const [tasksUpcoming, setTasksUpcoming] = useState<TaskCard[]>([]);
 
   useEffect(() => {
     getTasksList();
     getCategoryList();
+
+    const sortAndFilterTasks = () => {
+      const today = new Date().toLocaleDateString();
+      const tomorrow = new Date(Date.now() + 86400000).toLocaleDateString();
+
+      console.log('today', today);
+      console.log('tomorrow', tomorrow);
+
+      const tasksToday = taskListDate.filter((task) => task.dueDate === today);
+      const tasksTomorrow = taskListDate.filter(
+        (task) => task.dueDate === tomorrow,
+      );
+      const tasksUpcoming = taskListDate.filter(
+        (task) => task.dueDate !== today && task.dueDate !== tomorrow,
+      );
+
+      setTasksToday(tasksToday);
+      setTasksTomorrow(tasksTomorrow);
+      setTasksUpcoming(tasksUpcoming);
+    };
+
+    sortAndFilterTasks();
   }, [taskListDate, categoryListDate]);
 
   return (
@@ -22,14 +48,14 @@ const MainPage = () => {
         <TaskList
           day='Today'
           date={formattedDateToday}
-          taskListDate={taskListDate}
+          taskListDate={tasksToday}
         />
         <TaskList
           day='Tomorrow'
           date={formattedDateTomorrow}
-          taskListDate={taskListDate}
+          taskListDate={tasksTomorrow}
         />
-        <TaskList day='Upcoming' taskListDate={taskListDate} />
+        <TaskList day='Upcoming' taskListDate={tasksUpcoming} />
       </div>
     </div>
   );
