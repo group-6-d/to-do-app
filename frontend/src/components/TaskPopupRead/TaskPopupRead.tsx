14d8a40ffe6ee44245ac215ab7a0e18b335e072d
Elena <64535091+elen-oz@users.noreply.github.com>
2024-03-12 16:51:23 +0100

    Add favicon/elena (#35)
    * feat: add favicon
    
    * feat: change color: from gray to stone

--- frontend/src/components/TaskPopupRead/TaskPopupRead.tsx
+++ frontend/src/components/TaskPopupRead/TaskPopupRead.tsx
@@ -23,12 +23,12 @@ const TaskPopupRead: FC<TaskPopupProps> = ({ task, closeTaskPopup }) => {
       className='animate-fade fixed inset-0 flex h-full w-full cursor-pointer items-center justify-center bg-black bg-opacity-70'
       onClick={closePopup}
     >
-      <div className='h-screen/80 relative grid w-[300px] content-start overflow-auto rounded-2xl bg-white p-5 md:w-[700px] md:p-10 dark:bg-stone-800'>
+      <div className='h-screen/80 relative grid w-[300px] cursor-default content-start overflow-auto rounded-2xl bg-white p-5 md:w-[700px] md:p-10 dark:bg-stone-800'>
         <section
-          className='mb-6 flex w-full justify-end
-  md:mb-12'
+          className='mb-6 flex w-full
+  justify-end md:mb-12'
         >
-          <ul className='grid grid-cols-4 gap-3 text-xl text-gray-600 md:gap-6 md:text-2xl'>
+          <ul className='grid grid-cols-4 gap-3 text-xl text-stone-600 md:gap-6 md:text-2xl'>
             {!task?.isDone && (
               <li>
                 <IoMdCheckmarkCircleOutline className='icon' />
@@ -46,38 +46,38 @@ const TaskPopupRead: FC<TaskPopupProps> = ({ task, closeTaskPopup }) => {
           </ul>
         </section>
         <div
-          className={`mb-3 flex items-center text-sm font-medium uppercase md:mb-4 md:text-xl ${priorityColor}`}
+          className={`mb-3 flex  items-center text-sm font-medium uppercase md:mb-4 md:text-xl ${priorityColor}`}
         >
           <TbPointFilled className='text-4xl' />
           <p className=''>{`${task?.priority} priority`}</p>
         </div>
-        <h1 className='mb-2 text-xl font-semibold md:mb-6 md:text-4xl'>
+        <h1 className='mb-2  text-xl font-semibold md:mb-6 md:text-4xl'>
           {task?.title}
         </h1>
         <div className='mb-10 mt-6 grid grid-cols-2 items-end gap-y-4 md:mb-16 md:w-96 md:gap-y-5'>
-          <p className='text-[12px] uppercase text-gray-500 md:text-lg'>
+          <p className='text-[12px] uppercase text-stone-500 md:text-lg'>
             Due date
           </p>
           <p className='font-medium md:text-xl'>{task?.dueDate}</p>
-          <p className='text-[12px] uppercase text-gray-500 md:text-lg'>
+          <p className='text-[12px] uppercase text-stone-500 md:text-lg'>
             Category
           </p>
           <p className='text-accent font-medium md:text-xl'>{task?.category}</p>
-          <p className='text-[12px] uppercase text-gray-500 md:text-lg'>
+          <p className='text-[12px] uppercase text-stone-500 md:text-lg'>
             Status
           </p>
           {!task?.isDone && (
-            <p className='bg-coral w-24 rounded-full text-center font-medium uppercase text-white hover:bg-opacity-80 md:w-32 md:text-xl'>
+            <p className='text-coral font-medium uppercase text-white hover:text-opacity-80 md:text-xl'>
               to do
             </p>
           )}
           {task?.isDone && (
-            <p className='w-24 rounded-full text-center font-medium uppercase text-gray-500 hover:bg-opacity-80 md:w-32 md:text-xl'>
+            <p className='font-medium uppercase text-stone-500 hover:text-opacity-80 md:text-xl'>
               done
             </p>
           )}
         </div>
-        <p className='text-[12px] uppercase text-gray-500 md:text-lg'>
+        <p className='text-[12px] uppercase text-stone-500 md:text-lg'>
           Description
         </p>
         <p className='my-2 md:my-3 md:text-2xl'>{task?.description}</p>
