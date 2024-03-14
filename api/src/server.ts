fe75b99ce5bb98800a446551befcff6b784f4e71
Jinjing Wu <wujinjing25@gmail.com>
2024-03-14 09:30:04 +0100

    feat: user registration (#38)
    * feat: user register
    
    * feat: reorgainze the code

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,29 +1,29 @@
 import dotenv from 'dotenv';
 import { destroyDBConnections } from './client/db';
-import app from './app';
+import express from './express';
 
 dotenv.config();
 
-const port = process.env.PORT;
+const main = () => {
+  const port = process.env.PORT;
 
-// test db connection
+  express.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+  const shutdown = async (signal: string) => {
+    await destroyDBConnections();
+    console.info(`[${signal}] This service is shutting down!`);
+    process.exit(0);
+  };
 
-const shutdown = async (signal: string) => {
-  await destroyDBConnections();
-  console.info(`[${signal}] This service is shutting down!`);
-  process.exit(0);
+  process.on('SIGTERM', async () => {
+    await shutdown('SIGTERM');
+  });
+
+  process.on('SIGINT', async () => {
+    await shutdown('SIGINT');
+  });
 };
 
-process.on('SIGTERM', async () => {
-  await shutdown('SIGTERM');
-});
-
-process.on('SIGINT', async () => {
-  await shutdown('SIGINT');
-});
-
-export default app;
+main();
---
+++ api/src/types.ts
@@ -0,0 +1,3 @@
+export interface GeneralReturn<T> {
+  result?: Partial<T>;
+}
