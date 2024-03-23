1254a4523c1a524df1efcf6a926e8738b6beeb85
elen-oz <ozerova.lenka@gmail.com>
2024-03-23 17:42:54 +0100

    fix: styles

--- frontend/src/components/TaskList/TaskList.tsx
+++ frontend/src/components/TaskList/TaskList.tsx
@@ -25,19 +25,25 @@ const TaskList: FC<TaskListProps> = ({ taskList, day }) => {
 
   return (
     <>
-      <ul className='min-w-[290px] md:overflow-hidden'>
-        <h4 className='rounded-t-2xl bg-white p-4 font-semibold dark:bg-stone-800 '>
+      <ul className='min-w-[330px] md:overflow-hidden'>
+        <h4 className='mx-3 rounded-t-2xl bg-white p-4 font-semibold dark:bg-stone-800'>
           {day}
         </h4>
-        <div className='h-full md:overflow-scroll'>
-          <div className='mb-6 rounded-b-2xl bg-white pb-2 shadow-xl md:mb-[5rem] dark:bg-stone-800 '>
-            {taskList.map((task) => (
-              <TaskListItem
-                key={task.id}
-                task={task}
-                onClick={() => openTaskPopup(task)}
-              />
-            ))}
+        <div className='h-full px-3 md:overflow-scroll'>
+          <div className='mb-6 rounded-b-2xl bg-white pb-2  shadow-xl md:mb-[5rem] dark:bg-stone-800'>
+            {!taskList && (
+              <li className='hover:dark:border-accent mx-2 mb-3 flex cursor-default flex-col justify-between rounded-lg border-[1px] border-stone-200 bg-white px-4  py-2 dark:border-stone-700 dark:bg-stone-800'>
+                No tasks
+              </li>
+            )}
+            {taskList &&
+              taskList.map((task) => (
+                <TaskListItem
+                  key={task.id}
+                  task={task}
+                  onClick={() => openTaskPopup(task)}
+                />
+              ))}
           </div>
         </div>
       </ul>
