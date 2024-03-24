d44113325f1054c4aabb1cb93d04858fff51fcf0
elen-oz <ozerova.lenka@gmail.com>
2024-03-24 11:58:17 +0100

    refactor: add mapping by priority

--- frontend/src/components/SideBar/SideBar.tsx
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -15,6 +15,13 @@ import { LiaPlusSolid } from 'react-icons/lia';
 import TaskPopupNew from '../TaskPopupNew';
 import { useCategoriesContext } from '../../context/CategoryContext';
 import Category from '../../models/Category';
+import SideBarPriorityFilterItem from '../SideBarPriorityFilterItem/SideBarPriorityFilterItem';
+
+const priorityItem = [
+  { name: 'High', color: 'coral' },
+  { name: 'Medium', color: 'yellow' },
+  { name: 'low', color: 'purple' },
+];
 
 const SideBar: FC = () => {
   const [isTaskPopupOpen, setIsTaskPopupOpen] = useState(false);
@@ -31,7 +38,7 @@ const SideBar: FC = () => {
   };
 
   return (
-    <aside className='h-fit min-w-[180px] rounded-br-3xl rounded-tr-3xl bg-white py-6 dark:bg-stone-800'>
+    <aside className='h-fit min-w-fit rounded-br-3xl rounded-tr-3xl bg-white py-6 dark:bg-stone-800'>
       <button className='bg-accent hover:bg-accentDark mx-auto mb-4 flex items-center whitespace-nowrap rounded-full px-8 py-4 text-white'>
         <div
           onClick={() => openTaskPopup()}
@@ -42,58 +49,19 @@ const SideBar: FC = () => {
         </div>
       </button>
 
-      {/* <ul className='mx-2 mb-3 rounded-lg border-[1px] border-stone-200 bg-white pb-1 dark:border-stone-700 dark:bg-stone-800'>
+      <ul className='mx-2 mb-3 rounded-lg border-[1px] border-stone-200 bg-white pb-1 dark:border-stone-700 dark:bg-stone-800'>
         <div className='p-2'>Priority:</div>
         <div className='flex'>
-          <li className='items-middle flex justify-between gap-2 rounded-xl p-2 hover:bg-stone-100 hover:dark:bg-stone-700'>
-            <input
-              type='checkbox'
-              id='high'
-              name='high'
-              className='checked:bg-coral h-5 w-5 appearance-none rounded-full bg-white hover:cursor-pointer'
-            />
-            <label
-              htmlFor='high'
-              className='m-0 text-sm  text-stone-900 hover:cursor-pointer dark:text-stone-100'
-            >
-              High
-            </label>
-          </li>
-          <li className='items-middle flex justify-between gap-2 rounded-xl p-2 hover:bg-stone-100 hover:dark:bg-stone-700'>
-            <input
-              type='checkbox'
-              id='middle'
-              name='middle'
-              className='checked:bg-yellow h-5 w-5 appearance-none rounded-full bg-white hover:cursor-pointer'
-            />
-            <label
-              htmlFor='middle'
-              className='m-0 text-sm text-stone-900 hover:cursor-pointer dark:text-stone-100'
-            >
-              Middle
-            </label>
-          </li>
-          <li className='items-middle flex justify-between gap-2 rounded-xl p-2 hover:bg-stone-100 hover:dark:bg-stone-700'>
-            <input
-              type='checkbox'
-              id='low'
-              name='low'
-              className='checked:bg-purple h-5 w-5 appearance-none rounded-full bg-white hover:cursor-pointer'
-            />
-            <label
-              htmlFor='low'
-              className='m-0 text-sm text-stone-900 hover:cursor-pointer dark:text-stone-100'
-            >
-              Low
-            </label>
-          </li>
+          {priorityItem.map((item) => (
+            <SideBarPriorityFilterItem name={item.name} color={item.color} />
+          ))}
         </div>
-      </ul> */}
+      </ul>
 
       <ul className=''>
         {arrСategories &&
           arrСategories.map((category) => (
-            <SideBarItem key={category.id} category={category} />
+            <SideBarItem key={category.name} category={category} />
           ))}
       </ul>
       {isTaskPopupOpen && (
